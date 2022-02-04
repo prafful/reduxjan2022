9a66d562eb4056af29f812c87efcd4cd3526ea66
Prafful Daga <prraful@gmail.com>
2022-02-04 12:03:28 +0530

    redux app

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './App.css';
+import FriendList from './containers/friendlist';
+
+function App() {
+  return (
+    <div className="App">
+     
+     < FriendList></FriendList>
+
+    </div>
+  );
+}
+
+export default App;
